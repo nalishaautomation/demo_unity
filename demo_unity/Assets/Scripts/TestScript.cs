22205bbda666bae4731e916e8f5db87b23f6fd38
nalishaautomation <projects@nalishaautomation.com>
2021-12-09 16:22:12 +0530

    lecture2
    getting game objects and manulplating it properties

--- demo_unity/Assets/Scripts/TestScript.cs
+++ demo_unity/Assets/Scripts/TestScript.cs
@@ -6,15 +6,72 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     public int pranav = 5;
+
+    public int[] list;
+
+    public GameObject[] Newobject;
+
+    public Color[] NewColors;
+
+    GameObject[] objAll;
+
+    GameObject findSphere;
+
+    Rigidbody rbNew;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // print(list[1]);
+
+        //Destroy(Newobject[2]);
+        /* int i = 0;
+         while (i < 5)
+         {
+
+             for (int j = 0; j < 3; j++)
+             {
+                 Newobject[1].GetComponent<Renderer>().material.color = NewColors[j + 1 ];
+                 Newobject[0].GetComponent<Renderer>().material.color = NewColors[j + 2];
+                 Newobject[2].GetComponent<Renderer>().material.color = NewColors[j + 3 ];
+             }
+             i++;
+         }*/
+        /*
+                foreach(GameObject go in Newobject)
+                {
+                    //Destroy(go);
+                    go.SetActive(false);
+                }*/
+
+        //findSphere = GameObject.Find("Sphere/Sphere/Sphere");
+        //findSphere = GameObject.FindWithTag("Destroy");
+        /*
+                objAll = GameObject.FindGameObjectsWithTag("Destroy");
+
+                foreach(GameObject obj in objAll)
+                {
+                    Destroy(obj);
+                }
+                */
+
+        rbNew = GetComponent<Rigidbody>();
+        rbNew.useGravity = true;
+        /*rbNew.mass = 5;
+
+        TotalScore totalScore;
+        totalScore = GetComponent<TotalScore>();        // for getting the porperties value from another script
+        totalScore.score = 100;*/
+
+        GameObject.Find("Cylinder").GetComponent<TotalScore>().score = 500;
+
+        GameObject.Find("Cylinder").GetComponent<Rigidbody>().useGravity = true; 
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        print("Hi Pranav");
+        
+        //print("Hi Pranav");
     }
 }
---
+++ demo_unity/Assets/Scripts/TotalScore.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalScore : MonoBehaviour
+{
+
+    public int score = 90;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
